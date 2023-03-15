befc69b83710516dcb7b672be237066efb712816
anitamalina <anitatranberg@gmail.com>
2023-03-15 14:30:58 +0100

    delete component from App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.css";
-import data from "./data/data.json";
-import CategoryItem from "./components/CategoryItem";
 import { PersonComponent } from "./PersonComponent";
 
 function App() {
   return (
     <div>
       <PersonComponent />
-      <CategoryItem
-        source={data[0].object_url}
-        labelName={data[0].label_name}
-      />
-      <CategoryItem
-        source={data[0].object_url}
-        labelName={data[0].label_name}
-      />
     </div>
   );
 }
