b80baa0e8b774c58ebd356692991c89038db762c
anitamalina <anitatranberg@gmail.com>
2023-03-16 11:39:52 +0100

    image-grid suggestion

--- src/components/ImageItem/ImageItem.js
+++ src/components/ImageItem/ImageItem.js
@@ -2,13 +2,8 @@ import "./ImageItem.css";
 
 export default function ImageItem(props) {
   return (
-    <div className="image-container">
-      <img
-        className="image"
-        src={props.source}
-        alt={`${props.labelName}`}
-        onClick={props.handleClick}
-      />
+    <div className="image-container" onClick={props.handleClick}>
+      <img className="image" src={props.source} alt={`${props.title}`} />
       <div className="overlay">
         <p className="text-overlay">{props.title}</p>
       </div>
