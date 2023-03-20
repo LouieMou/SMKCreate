f90d7880d29383b5daf769402f6d2ea4e54f70d7
Johanne Engel Aaen <jaae@itu.dk>
2023-03-20 14:37:30 +0100

    heading and frame

---
+++ src/components/FrontPageHeading.js
@@ -0,0 +1,13 @@
+import "./FrontPageHeading.css";
+export default function FrontPageHeading(props) {
+  const { color } = props;
+  const textColor = {
+    color: color || "black",
+  };
+  return (
+    <div className="heading-container" style={textColor}>
+      <h1>What would you like to explore?</h1>
+      <h2>Choose a category</h2>
+    </div>
+  );
+}
