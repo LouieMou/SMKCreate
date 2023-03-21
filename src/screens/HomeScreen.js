d65d09df9cd86673bd4c254c4120cd03644d007f
Johanne Engel Aaen <jaae@itu.dk>
2023-03-21 15:07:20 +0100

    home screen updated

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 /* Component */
-import { PersonComponent } from '../PersonComponent';
+import Frame from "../components/FrontPageFrame/Frame";
+import FrontPageHeading from "../components/Headings/FrontPageHeading";
+import FrontPageGrid from "../components/FrontPageGrid/FrontPageGrid";
 /* Styles */
-import './HomeScreen.css';
+import "./HomeScreen.css";
+
+import data from "../data/data.json";
 
 function HomeScreen(props) {
-    return (
-        <div>
-            <PersonComponent></PersonComponent>
-        </div>
-    );
+  const textColor = data[1].suggested_bg_color;
+  return (
+    <div className="frontpage-container">
+      <Frame>
+        <FrontPageHeading color={textColor} />
+        <FrontPageGrid data={data} />
+      </Frame>
+    </div>
+  );
 }
 
 export default HomeScreen;
