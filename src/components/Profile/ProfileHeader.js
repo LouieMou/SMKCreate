f23b53d86e02371776adbdca612e4f4b3f45355b
anitamalina <anitatranberg@gmail.com>
2023-04-11 13:52:41 +0200

    profile works I think

--- src/components/Profile/ProfileHeader.js
+++ src/components/Profile/ProfileHeader.js
@@ -1,18 +1,14 @@
-import { useState } from "react";
-
 /* Styles */
 import "./ProfileHeader.css";
 
 export default function ProfileHeader(props) {
-  const [name, setName] = useState(props.name);
-
   function getFirstLetter() {
     const firstLetter = props.name.charAt(0);
     return firstLetter;
   }
 
   function handleNameChange(event) {
-    setName(event.target.value);
+    props.setName(event.target.value);
   }
 
   return (
@@ -23,8 +19,9 @@ export default function ProfileHeader(props) {
       <input
         className="profile-name"
         type="text"
-        value={name}
+        value={props.name}
         onChange={handleNameChange}
+        required
       />
     </div>
   );
