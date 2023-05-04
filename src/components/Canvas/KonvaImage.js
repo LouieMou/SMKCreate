b9e79bc9fc05f94ebb0cd52033212338c29e0a4a
anitamalina <anitatranberg@gmail.com>
2023-05-04 18:10:16 +0200

    rotate works

--- src/components/Canvas/KonvaImage.js
+++ src/components/Canvas/KonvaImage.js
@@ -1,45 +1,8 @@
 import useImage from "use-image";
+import React from "react";
 import { Image, Transformer } from "react-konva";
-import { useState, useCallback } from "react";
 
 export default function KonvaImage({ image, setImagesOnLayer }) {
-  const [img] = useImage(image.src);
-  const [isSelected, setIsSelected] = useState(false);
-
-  const handleSelect = useCallback(() => {
-    console.log("handleSelect from clcik");
-    setIsSelected(true);
-  }, []);
-
-  const handleDeselect = useCallback(() => {
-    setIsSelected(false);
-  }, []);
-
-  const handleTransform = useCallback(
-    (e) => {
-      const node = e.target;
-      const scaleX = node.scaleX();
-      const scaleY = node.scaleY();
-
-      // update the image position to align with the transformer's position
-      const dx = node.width() * scaleX * 0.5;
-      const dy = node.height() * scaleY * 0.5;
-      const x = node.x() - dx;
-      const y = node.y() - dy;
-
-      setImagesOnLayer((imageOnLayer) =>
-        imageOnLayer.map((image) => {
-          if (image.id === node.id()) {
-            return { ...image, x, y, scaleX, scaleY };
-          } else {
-            return image;
-          }
-        })
-      );
-    },
-    [setImagesOnLayer]
-  );
-
   const handleDragEnd = (e) => {
     let id = e.target.id();
     const newX = e.target.x();
@@ -55,7 +18,41 @@ export default function KonvaImage({ image, setImagesOnLayer }) {
       })
     );
   };
-  const imgId = image.id;
+
+  const handleTransform = (e) => {
+    let id = e.target.parent.id();
+    const node = e.target.parent;
+    const scaleX = node.scaleX();
+    const scaleY = node.scaleY();
+
+    setImagesOnLayer((imageOnLayer) =>
+      imageOnLayer.map((image) => {
+        if (image.id === id) {
+          return { ...image, scaleX: scaleX, scaleY: scaleY };
+        } else {
+          return image;
+        }
+      })
+    );
+  };
+
+  const [img] = useImage(image.src);
+  let imgId = image.id;
+  const transformerRef = React.useRef(null);
+
+  const handleSelect = (e) => {
+    const clickedId = e.target.id();
+    const isSelected = transformerRef.current
+      .nodes()
+      .some((node) => node.id() === clickedId);
+
+    if (isSelected) {
+      transformerRef.current.nodes([]);
+    } else {
+      transformerRef.current.nodes([e.target]);
+    }
+  };
+
   return (
     <>
       <Image
@@ -68,27 +65,16 @@ export default function KonvaImage({ image, setImagesOnLayer }) {
         draggable
         onDragEnd={handleDragEnd}
         onClick={handleSelect}
-        onTap={handleSelect}
-        onMouseUp={handleSelect}
-        onTouchEnd={handleSelect}
+      />
+      <Transformer
+        anchorSize={10}
+        borderDash={[6, 2]}
+        borderEnabled
+        rotationEnabled={false}
+        keepRatio={false}
+        ref={transformerRef}
         onTransformEnd={handleTransform}
       />
-      {isSelected && (
-        <Transformer
-          anchorSize={10}
-          borderDash={[6, 2]}
-          borderEnabled
-          rotationEnabled={false}
-          keepRatio={false}
-          ref={(node) => {
-            // manually attach transformer node to image node
-            const imgNode = node.getParent().findOne(`#${imgId}`);
-            if (imgNode) {
-              node.nodes([imgNode]);
-            }
-          }}
-        />
-      )}
     </>
   );
 }
