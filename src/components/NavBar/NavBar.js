372df3246cdfa124f31ff02216a4f79df0b2e2a3
Johanne Engel Aaen <jaae@itu.dk>
2023-08-09 14:19:38 +0200

    NavBar update with cat-logo & heart gone on canvas

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -39,55 +39,101 @@ function NavBar(props) {
   }
 
   return (
-    <nav
-      className="navbar-container"
-      style={{ backgroundColor: props.bgColor }}
-    >
-      {SMKlogoSize === "small" ? (
-        <NavIcon
-          src={`/icons/create_logo_${navBarTextColor}.svg`}
-          handleClick={() => navigateTo("/home")}
-          iconWidth="118px"
-        />
+    <div>
+      {location.pathname === "/canvas" ? (
+        <nav
+          className="navbar-container"
+          style={{ backgroundColor: props.bgColor }}
+        >
+          {SMKlogoSize === "small" ? (
+            <NavIcon
+              src={`/icons/create_logo_${navBarTextColor}.svg`}
+              handleClick={() => navigateTo("/home")}
+              iconWidth="118px"
+            />
+          ) : (
+            <NavIcon
+              src={`/icons/create_logo_${navBarTextColor}.svg`}
+              handleClick={() => navigateTo("/home")}
+            />
+          )}
+
+          <div className="rightside-icons">
+            <NavIcon
+              img_container_style={"nav-icon-img"}
+              src={`/icons/back_icon_${navBarTextColor}.svg`}
+              handleClick={() => goBack()}
+            />
+            <NavLinkIcon
+              filledIcon={`/icons/categories_icon_${navBarTextColor}_filled.svg`}
+              unfilledIcon={`/icons/categories_icon_${navBarTextColor}_unfilled.svg`}
+              path="/categories"
+            />
+
+            <NavLinkIcon
+              filledIcon={`/icons/create_${navBarTextColor}_filled.svg`}
+              unfilledIcon={`/icons/create_${navBarTextColor}_unfilled.svg`}
+              path="/canvas"
+            />
+
+            <NavLinkIcon
+              filledIcon={`/icons/profile_filled_${navBarTextColor}.svg`}
+              unfilledIcon={`/icons/profile_unfilled_${navBarTextColor}.svg`}
+              path="/profile"
+            />
+          </div>
+        </nav>
       ) : (
-        <NavIcon
-          src={`/icons/create_logo_${navBarTextColor}.svg`}
-          handleClick={() => navigateTo("/home")}
-        />
+        <nav
+          className="navbar-container"
+          style={{ backgroundColor: props.bgColor }}
+        >
+          {SMKlogoSize === "small" ? (
+            <NavIcon
+              src={`/icons/create_logo_${navBarTextColor}.svg`}
+              handleClick={() => navigateTo("/home")}
+              iconWidth="118px"
+            />
+          ) : (
+            <NavIcon
+              src={`/icons/create_logo_${navBarTextColor}.svg`}
+              handleClick={() => navigateTo("/home")}
+            />
+          )}
+
+          <div className="rightside-icons">
+            <NavIcon
+              img_container_style={"nav-icon-img"}
+              src={`/icons/back_icon_${navBarTextColor}.svg`}
+              handleClick={() => goBack()}
+            />
+            <NavLinkIcon
+              filledIcon={`/icons/categories_icon_${navBarTextColor}_filled.svg`}
+              unfilledIcon={`/icons/categories_icon_${navBarTextColor}_unfilled.svg`}
+              path="/categories"
+            />
+
+            <NavIcon
+              img_container_style={"nav-icon-img"}
+              src={`/icons/heart_unfilled_${navBarTextColor}.svg`}
+              handleClick={props.openFavoriteList}
+            />
+
+            <NavLinkIcon
+              filledIcon={`/icons/create_${navBarTextColor}_filled.svg`}
+              unfilledIcon={`/icons/create_${navBarTextColor}_unfilled.svg`}
+              path="/canvas"
+            />
+
+            <NavLinkIcon
+              filledIcon={`/icons/profile_filled_${navBarTextColor}.svg`}
+              unfilledIcon={`/icons/profile_unfilled_${navBarTextColor}.svg`}
+              path="/profile"
+            />
+          </div>
+        </nav>
       )}
-
-      <div className="rightside-icons">
-        <NavIcon
-          img_container_style={"nav-icon-img"}
-          src={`/icons/back_icon_${navBarTextColor}.svg`}
-          handleClick={() => goBack()}
-        />
-        {location.pathname === "/canvas" ? (
-          <NavIcon
-            img_container_style={"nav-icon-img"}
-            src={`/icons/heart_filled_${navBarTextColor}.svg`}
-          />
-        ) : (
-          <NavIcon
-            img_container_style={"nav-icon-img"}
-            src={`/icons/heart_unfilled_${navBarTextColor}.svg`}
-            handleClick={props.openFavoriteList}
-          />
-        )}
-
-        <NavLinkIcon
-          filledIcon={`/icons/create_${navBarTextColor}_filled.svg`}
-          unfilledIcon={`/icons/create_${navBarTextColor}_unfilled.svg`}
-          path="/canvas"
-        />
-
-        <NavLinkIcon
-          filledIcon={`/icons/profile_filled_${navBarTextColor}.svg`}
-          unfilledIcon={`/icons/profile_unfilled_${navBarTextColor}.svg`}
-          path="/profile"
-        />
-      </div>
-    </nav>
+    </div>
   );
 }
 
